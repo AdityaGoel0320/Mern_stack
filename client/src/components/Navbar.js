2af29375049eef91d5561c729ccb5d098a309102
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-07-16 17:05:41 +0530

    navbar completed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import 'bootstrap/dist/css/bootstrap.css';
 export default function Navbar() {
     return (
         <>
@@ -11,7 +10,7 @@ export default function Navbar() {
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav  ml-auto">
+                    <ul className="navbar-nav  ms-auto">
                         <li className="nav-item active">
                             <a className="nav-link " href="#">Home</a>
                         </li>
