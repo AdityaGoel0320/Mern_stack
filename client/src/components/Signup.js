09745c61e54f99586fb2dfa1e984de7b15d019d9
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-07-18 02:10:38 +0530

    data of sign up form stroed in variable in react

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink, Routes, Route, Link, BrowserRouter } from "react-router-dom"
 import randomImg from "../images/download.jpg"
 export default function Signup() {
+
+
+  // now storing from data in variable usinf useState
+  const [User, setUser] = useState(
+    {
+      name: "",
+      email: "",
+      phone: "",
+      work: "",
+      password: "",
+      cpassword: "",
+
+    }
+  )
+
+
+  let name, value;
+  let handleInputs = (e) => {
+    console.log(e)
+    name = e.target.name;
+    value = e.target.value;
+
+    setUser({ ...User, [name]: value })
+
+  }
+
   return (
+
+
     <>
 
       <div className='extra'>Signup</div>
@@ -14,32 +42,32 @@ export default function Signup() {
         <form action="register" method="POST">
           <label htmlFor="">Name</label>
           <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-            autoComplete='off' placeholder="Name" name="name" />
+            autoComplete='off' value={User.name} onChange={handleInputs} placeholder="Name" name="name" />
 
           <label htmlFor="">email</label>
           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-            autoComplete='off' placeholder="email" name="email" />
+            autoComplete='off' value={User.email} onChange={handleInputs} placeholder="email" name="email" />
 
 
           <label htmlFor="">phone</label>
           <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-            autoComplete='off' placeholder="phone number" name="phone" />
+            autoComplete='off' value={User.phone} onChange={handleInputs} placeholder="phone number" name="phone" />
 
 
           <label htmlFor="">profession</label>
           <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-            autoComplete='off' placeholder="profession" name="profession" />
+            autoComplete='off' value={User.profession} onChange={handleInputs} placeholder="profession" name="profession" />
 
 
 
           <label htmlFor="">password</label>
           <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-            autoComplete='off' placeholder="password" name="password" />
+            autoComplete='off' value={User.password} onChange={handleInputs} placeholder="password" name="password" />
 
           <label htmlFor="">cpassword</label>
 
           <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-            autoComplete='off' placeholder="confirmpassword" name="cpassword" />
+            autoComplete='off' value={User.cpassword} onChange={handleInputs} placeholder="confirmpassword" name="cpassword" />
 
           <button type="submit" name='submit'>register</button>
         </form>
