e3e3878e99f75da3d0297c5d89e1f9b7dad053ef
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-07-26 23:15:23 +0530

    some changes

--- server/src/route/auth.js
+++ server/src/route/auth.js
@@ -1,143 +1,97 @@
-let express = require("express");
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-let router = express.Router();
-let bcrypt = require("bcryptjs")
-
-
-let jwt = require("jsonwebtoken")
-
-
-
-
-
-require("../db/conn")
-let User = require("../model/userSchema")
+require("../db/conn");
+const User = require("../model/userSchema");
 
 router.get("/", (req, res) => {
-    res.send("Mern Stack Project Home Page")
-})
-
+  res.send("Mern Stack Project Home Page");
+});
 
 router.get("/register", (req, res) => {
-    // console.log(req.body)
-    res.send("register page using get ")
-})
-
-
+  res.send("register page using get");
+});
 
 router.post("/register", async (req, res) => {
+  try {
+    const { name, email, phone, work, password, cpassword } = req.body;
 
-
-    try {
-
-
-        let { name, email, phone, work, password, cpassword } = req.body;
-
-        // checking user didnt make any empty field
-        if (!name || !email || !phone || !work || !password || !cpassword) {
-            return res.status(422).send("enter details properly")
-        }
-        if (password != cpassword) {
-            return res.status(422).send("password are not maching")
-
-        } else {
-
-            let userExist = await User.findOne({ email: email })
-            if (userExist == true) {
-                return res.status(422).json({ error: "user already register" })
-            }
-
-
-            let newUserDetails = new User({ name, email, phone, work, password, cpassword })
-
-
-            // now here wer have to hash our pass and cpass so  using bcryptjs
-
-
-            // as in schema file we make a fnc pre which automatically run before saving as we make it as pre("save") thus before saving we want hashing we wrte acc to it in pre
-
-
-            await newUserDetails.save();
-
-            return res.status(201).json({ message: "user registerd successfully" })
-        }
-
-
-
-    } catch (error) {
-        console.log(error)
-        // console.log("error in registration post request")
+    // checking user didn't leave any empty fields
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+      return res.status(422).send("Enter all details properly");
     }
 
+    if (password !== cpassword) {
+      return res.status(422).send("Passwords do not match");
+    } else {
+      const userExist = await User.findOne({ email: email });
 
+      if (userExist) {
+        return res.status(422).json({ error: "User already registered" });
+      }
 
-})
+      // Hashing the password
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const newUserDetails = new User({
+        name,
+        email,
+        phone,
+        work,
+        password: hashedPassword, // Save hashed password to the database
+        cpassword: hashedPassword, // You may want to remove this field from the schema
+      });
 
-
+      await newUserDetails.save();
+      return res.status(201).json({ message: "User registered successfully" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error in registration");
+  }
+});
 
 router.get("/signin", (req, res) => {
-    res.send("welcome to sign in  page")
-})
-
+  res.send("Welcome to the sign-in page");
+});
 
 router.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-    try {
-        let { email, password } = req.body;
-
-        // checking given data is not empty
-        if (!email || !password) {
-            res.status(400).send("enter details propely for sigin ")
-        }
-
-        // a variable that store details if email match the databse
-        let userLogin = await User.findOne({ email: email })
-
-        if (userLogin) {
-            let passwordIsMatch = await bcrypt.compare(password, userLogin.password)
-
-            // as creating a jwt token so to make code small we make a fnc in databse 
-
-            let newToken = await userLogin.generateAuthToken()  ; 
-            console.log("new token is " + newToken)
-
-
-            // now storing this token in cookie
-            res.cookie("jetToken" , newToken , {
-                expires : new Date(Date.now() + 9999999999999) ,  //time in millisec
-                httpOnly : true 
-                
-            })
-
-
-
-            if (!passwordIsMatch) {
-                res.json("invalid credentials")
-            } else {
-                res.json("user login succesfully")
-            }
-        }
-
-        else {
-            res.json("user is not registerd")
-        }
-
-
-    } catch (error) {
-        console.log(error)
+    // Checking if the given data is not empty
+    if (!email || !password) {
+      return res.status(400).send("Enter details properly for signin");
     }
 
-})
+    const userLogin = await User.findOne({ email: email });
 
-// router.get("/contact", middleware, (req, res) => {
-//     res.send("Hello World from contact")
-// })
-// router.get("/signin", (req, res) => {
-//     res.send("Hello World from sign in")
-// })
-// router.get("/signup", (req, res) => {
-//     res.send("Hello World from sign up")
-// })
+    if (userLogin) {
+      const passwordIsMatch = await bcrypt.compare(password, userLogin.password);
 
+      if (!passwordIsMatch) {
+        return res.json("Invalid credentials");
+      } else {
+        // Creating and sending a JSON Web Token (JWT) upon successful login
+        const token = jwt.sign({ _id: userLogin._id }, "your_secret_key"); // Replace 'your_secret_key' with your actual secret key
+        console.log("new token is " + token);
 
-module.exports = router; 
+        // Storing the token in a cookie
+        res.cookie("jwtToken", token, {
+          expires: new Date(Date.now() + 9999999999999), // Expiry date for the cookie
+          httpOnly: true,
+        });
+
+        res.json("User login successful");
+      }
+    } else {
+      res.json("User is not registered");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error in signing in");
+  }
+});
+
+module.exports = router;
