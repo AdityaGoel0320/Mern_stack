11c14dfe909918be8890c1705de3c5266e141b15
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-07-26 15:30:42 +0530

    some edits

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -41,7 +41,7 @@ export default function Signup() {
     let { name, email, phone, work, password, cpassword } = obj;
     console.log(obj)
 
-    let response = await fetch("/register", {
+    let response = await fetch("http:/localhost:8000/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
